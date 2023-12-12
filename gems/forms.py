0c3cb532a7ffcb8fb28ecd2b76b3d86e9eda1226
Antonyeaster <>
2023-12-12 15:54:30 +0000

    Add username max length validator

--- gems/forms.py
+++ gems/forms.py
@@ -1,6 +1,9 @@
 from .models import Comment
 from django import forms
 from allauth.account.forms import SignupForm
+from django.core.validators import MaxLengthValidator
+
+""" https://stackoverflow.com/questions/50548685/how-to-add-max-length-to-allauth-username"""
 
 
 class CustomSignupForm(SignupForm):
@@ -9,6 +12,8 @@ class CustomSignupForm(SignupForm):
 
         self.fields['email'].required = True
         self.fields['email'].label = 'Email Address'
+        self.fields['username'].validators += [MaxLengthValidator(
+            20, "Username should be 20 characters or less")]
 
 
 class CommentForm(forms.ModelForm):
